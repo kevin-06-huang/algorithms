4ec1a5a35b4e34d59a888f1bdfda35940be6b45b
kevin-06-huang <kevin.06.huang@gmail.com>
2019-05-30 15:50:06 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Admin9
+ *
+ * Created on May 29, 2019, 10:08 AM
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout << "Hello, World!";
+    return 0;
+}
+
